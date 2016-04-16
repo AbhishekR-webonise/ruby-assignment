70cc0659a1573c18ca6ca5d9de5fefd032b55d19
Abhishek-webonise <you@example.com>
2016-04-16 16:46:27 +0530

    second commit

---
+++ sample2.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby -w
+a = 10
+b = 3 * a + 2
+printf("%d %d\n", a, b);
+
+
+b = "A string"
+c = 'Another String'
+print b + " and " + c + "\n"
+
---
+++ sample3.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby -w
+
+a = [ 45, 3, 19, 8 ]
+b = [ 'sam', 'max', 56, 98.9, 3, 10, 'jill' ]
+print (a + b).join(' '), "\n"
