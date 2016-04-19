7d9a3b44c0430bd77f8dcdd8cb936f65dfd00cbc
AbhishekRana95 <abhishek.rana@weboniselab.com>
2016-04-19 11:26:54 +0530

    require_relative
    Created a single new file and included all assignment files using require_relative

---
+++ Assignment1/ruby-assignments.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+
+puts '.......................ASSIGNMENT 1.......................'
+require_relative '1.rb'
+
+puts '.......................ASSIGNMENT 2.......................'
+require_relative '2.rb'
+
+puts '.......................ASSIGNMENT 3.......................'
+require_relative '3.rb'
+
+puts '.......................ASSIGNMENT 4.......................'
+require_relative '4.rb'
+
+puts '.......................ASSIGNMENT 5.......................'
+require_relative '5.rb'
+
+puts '.......................ASSIGNMENT 6.......................'
+require_relative '6.rb'
+
+puts '.......................ASSIGNMENT 7.......................'
+require_relative '7.rb'
+
+puts '.......................ASSIGNMENT 8.......................'
+require_relative '8.rb'
+
+puts '.......................ASSIGNMENT 9.......................'
+require_relative '9.rb'
